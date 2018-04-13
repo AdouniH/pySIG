cbd4dcc48d24e973f88556ee087b2558a5d0dfb3
adouniH <Houssemadouni11@gmail.com>
2018-04-13 22:47:00 +0200

    create the shp

--- ex1.py
+++ ex1.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 13 21:45:21 2018
+Created on Fri Apr 13 22:31:22 2018
 
 @author: houssem
 """
 
-from fiona.crs import from_epsg
-import fiona 
-
-shemaw={'geometry': 'Point','properties': {'id': 'int','categorie': 'str'}}
-
-with fiona.open('/home/houssem/postedetravail/pythonSIG/p/pySIG/output.shp', 'w',driver=source_driver,crs=from_epsg(3857), schema=source_schema) as source:
-    

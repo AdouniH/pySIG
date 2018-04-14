98642280abcaa63aa6766699706ff3b3ab383e59
adouniH <Houssemadouni11@gmail.com>
2018-04-14 15:58:42 +0200

    push dans le repository correcte

---
+++ SHPtoTAB.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 14 00:20:38 2018
+
+@author: houssem
+"""
+
--- ex1.py
+++ ex1.py
@@ -1,7 +1,96 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 13 22:31:22 2018
-
+Created on Fri Apr 13 21:45:21 2018
 @author: houssem
 """
 
+from fiona.crs import from_epsg
+import fiona 
+from shapely.geometry import Point,Polygon, mapping
+import csv
+
+        
+
+schemaw={'geometry': 'Point','properties': {'id': 'int','categorie': 'str'}}
+
+with fiona.open('/home/houssem/postedetravail/pythonSIG/p/pySIG/output.shp', 'w',driver='ESRI Shapefile',crs=from_epsg(27561), schema=schemaw) as output:
+    with open('/home/houssem/postedetravail/pythonSIG/p/pySIG/points_EPSG-27561.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            east=row['Easting']
+            east=east.replace(",", ".")
+            north=row['Northing']
+            north=north.replace(",", ".")
+            print(row['Group'],north, east)
+            if row['Group'][-1]=="1":
+                category='a'
+            elif row['Group'][-1]=="2":
+                category='b'
+            elif row['Group'][-1]=="3":
+                category='c'
+            point = Point(float(north),float(east),2)
+            output.write({'properties': {'id': 0,'categorie': category},'geometry': mapping(point)})
+        
+   
+schemawPolygon={'geometry': 'Polygon','properties': {'id': 'int','categorie': 'str'}}
+with fiona.open('/home/houssem/postedetravail/pythonSIG/p/pySIG/output2.shp', 'w',driver='ESRI Shapefile',crs=from_epsg(27561), schema=schemawPolygon) as output_polygones:
+    with fiona.open('/home/houssem/postedetravail/pythonSIG/p/pySIG/output.shp', 'r') as input_shp:
+        poly1=[]        
+        poly2=[] 
+        poly3=[] 
+        for line in input_shp:
+            if line["properties"]["categorie"]=='a':
+                poly1.append(line["geometry"]["coordinates"])
+        polygon_1 = Polygon(poly1) 
+        output_polygones.write({'properties': {'id': 0,'categorie': category},'geometry': mapping(polygon_1.convex_hull.buffer(1.0))})
+        for line in input_shp:
+            if line["properties"]["categorie"]=='b':
+                poly2.append(line["geometry"]["coordinates"])
+        polygon_2 = Polygon(poly2) 
+        output_polygones.write({'properties': {'id': 0,'categorie': category},'geometry': mapping(polygon_2.convex_hull.buffer(1.0))})
+        for line in input_shp:
+            if line["properties"]["categorie"]=='c':
+                poly3.append(line["geometry"]["coordinates"])
+        polygon_3 = Polygon(poly3) 
+        output_polygones.write({'properties': {'id': 0,'categorie': category},'geometry': mapping(polygon_3.convex_hull.buffer(1.0))})
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
